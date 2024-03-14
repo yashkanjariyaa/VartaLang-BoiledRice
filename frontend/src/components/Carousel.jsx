58fb6c1fa8b09676872ae70cbd885aa343358f1c
vedantnaik09 <127477068+vedantnaik09@users.noreply.github.com>
2024-03-15 02:24:28 +0530

    framer animation and glow effect added to login page

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,36 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {setIndex} from '../slices/generalSlice';
-
-
+import { setIndex } from '../slices/generalSlice';
+import './carousel.css'
 const Carousel = ({ images }) => {
-  const dispatch = useDispatch()
-  const currentIndex = useSelector((state)=>state.counter.index)
-  // const [currentIndex, setCurrentIndex] = useState(0);
+  const dispatch = useDispatch();
+  const currentIndex = useSelector((state) => state.counter.index);
   const totalSlides = images.length;
+  const [loadedIndex, setLoadedIndex] = useState(currentIndex);
 
   useEffect(() => {
     const interval = setInterval(() => {
       dispatch(setIndex((currentIndex + 1) % totalSlides));
-    }, 2000); // Change slide every 5 seconds
+    }, 2000); // Change slide every 2 seconds
 
     return () => clearInterval(interval);
-  }, [currentIndex, totalSlides]);
+  }, [currentIndex, totalSlides, dispatch]);
+
+  useEffect(() => {
+    setLoadedIndex(currentIndex);
+  }, [currentIndex]);
 
   return (
-    <div className="">
-      <div className="carousel">
-        <div className="carousel-inner">
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className={`carousel-item ${index === currentIndex ? 'block' : 'hidden'}`}
-            >
-              <img src={image} alt={`Slide ${index + 1}`} className='w-80 mx-auto' />
-            </div>
-          ))}
+    <div className="relative w-80 h-80">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ${index === loadedIndex ? 'opacity-100' : 'opacity-0'}`}
+        >
+          <img 
+
+            src={image}
+            alt={`Slide ${index + 1}`}
+            className='w-full h-full object-cover carousel-image'
+            onLoad={() => setLoadedIndex(currentIndex)} // Trigger re-render when image is loaded
+            draggable="false"
+          />
         </div>
-      </div>
+      ))}
     </div>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Carousel from "../components/Carousel";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion"
 
+import { useSelector } from "react-redux";
+
+import "../assets/login.css";
+
+import googleLogo from "../assets/google.svg";
 const image1 =
   "https://static.vecteezy.com/system/resources/previews/010/842/688/original/3d-illustration-ethereum-blockchain-png.png";
 const image2 = "https://cdn-icons-png.flaticon.com/512/8757/8757988.png";
@@ -8,26 +15,110 @@ const image3 = "https://cdn-icons-png.flaticon.com/512/2091/2091665.png";
 
 const images = [image1, image2, image3];
 const Login = () => {
+  const currentIndex = useSelector((state) => state.counter.index);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    rememberMe: false,
+    address: "",
+    phoneNumber: "",
+    userIcon: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, files, checked } = e.target;
+    const newValue =
+      type === "file" ? files[0] : type === "checkbox" ? checked : value;
+
+    setFormData({
+      ...formData,
+      [name]: newValue,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here
+    console.log(formData);
+  };
+
   return (
     <div className="w-full h-[100vh] bg-primary-1 flex items-center">
       <div className="w-[65%] h-[90%] mx-auto rounded-2xl flex">
         <div className="flex flex-col w-[50%] bg-primary-2 rounded-l-2xl p-5 text-center">
-          
-          <div className="flex justify-center items-center mt-20">
-            <Carousel images={images} />
-          </div>
-          <span className="text-3xl  text-white my-5">
-            Blockchain website
-          </span>
-          <span className="text-2xl text-white my-3 w-3/4 mx-auto font-light">
-            Unlocking the Future: Empowering Possibilities with Blockchain Innovation
-          </span>
-          <span className="text-4xl text-white my-3">
-            Blockchain website
-          </span>
         
+          <motion.div className="flex justify-center items-center mt-20" drag whileHover={{scale:1.05}} dragSnapToOrigin="true" dragConstraints={{left:20,right:20,top:20,bottom:1}} dragElastic={0.25} initial={{y:-100}} animate={{y:0}} transition={{ delay:0.1}} dragTransition={{ bounceStiffness: 300, bounceDamping: 20 }}>
+            <Carousel images={images} />
+          </motion.div>
+          <motion.div className="" initial={{y:-100}} animate={{y:0}}>
+          <span className="text-3xl  text-white my-5 block">Blockchain website</span>
+          <span className="text-2xl text-white my-3 w-3/4 mx-auto font-light">
+            Unlocking the Future: Empowering Possibilities with Blockchain
+            Innovation
+          </span>
+          <div className="dots flex justify-center mt-5">
+            {images.map((_, index) => (
+              <svg
+                key={index}
+                className={`w-3 h-3 mx-1 ${
+                  currentIndex === index ? "text-white" : "text-gray-400"
+                }`}
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="10" cy="10" r="6" />
+              </svg>
+            ))}
+          </div>
+          </motion.div>
         </div>
-        <div className="w-[50%] rounded-r-2xl bg-white">Okay</div>
+
+        <div className="w-1/2 rounded-r-lg bg-primary-6 flex flex-col justify-center p-5">
+          <h1 className="mx-auto my-10 text-4xl">BlockChain Website</h1>
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Username or Email"
+              className="px-3 py-2 border h-14 rounded-lg"
+            />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password"
+              className="px-3 py-2 border h-14 rounded-lg"
+            />
+            <div className="flex justify-between items-center">
+              <a href="#" className="text-blue-800 hover:text-blue-900">
+                Forgot Password?
+              </a>
+              <button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+          <div className="flex items-center my-4">
+            <hr className="border-gray-600 border-1 w-full" />
+            <span className="text-gray-600 mx-4">or</span>
+            <hr className="border-gray-600 border-1 w-full" />
+          </div>
+          <button className="font-light rounded-xl p-2 w-1/2 mx-auto my-4 border-gray-400 border-[1px]">
+            <img src={googleLogo} alt="" className="w-8 inline-block" />
+            Sign In with Google
+          </button>
+          <span className="mt-4 mx-auto">
+            Are you new? <Link className="underline">Create an account</Link>
+          </span>
+        </div>
       </div>
     </div>
   );
