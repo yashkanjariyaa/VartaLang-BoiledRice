449220fac0035bf20f5cfe90c8fa8085125c8f51
Your Name <you@example.com>
2024-03-14 22:04:08 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Home } from './components/Home'
-import Login from './pages/Login'
-
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'; // Import from 'react-router-dom' instead of 'react'
+import { Home } from './components/Home';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <>
       {/* <Home/> */}
-      <Login/>
-
+      {/* <Login/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
