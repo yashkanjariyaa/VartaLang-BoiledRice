fd8748056ed9b6bc9b12dcbd645d62b6958b0b91
yashkanjariyaa <yashkanjariyaff@gmail.com>
2024-03-16 21:19:58 +0530

    small commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
-import generalSlice from './slices/generalSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import generalSlice from './slices/generalSlice';
 import activeUserSlice from "./redux/activeUserSlice";
 import chatsSlice from "./redux/chatsSlice";
 import profileSlice from "./redux/profileSlice";
 import searchSlice from "./redux/searchSlice";
+
 export const store = configureStore({
   reducer: {
-    counter: generalSlice,
-    activeUser: activeUserSlice,
-    profile: profileSlice,
-    search: searchSlice,
-    chats: chatsSlice,
+    counter: generalSlice.reducer,
+    activeUser: activeUserSlice.reducer,
+    profile: profileSlice.reducer,
+    search: searchSlice.reducer,
+    chats: chatsSlice.reducer,
   },
-})
+});
